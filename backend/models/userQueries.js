e9817c563b3b7739a05ce0ba759da0b51a6bf8e6
Josh Yeaton <yeaton.josh@gmail.com>
2025-08-07 08:41:19 -0400

    Add ability to create user

--- backend/models/userQueries.js
+++ backend/models/userQueries.js
@@ -1,9 +1,22 @@
+const { use } = require('passport');
 const { PrismaClient } = require('../generated/prisma');
 
 const prisma = new PrismaClient();
 
-async function createUser() {
-    
+async function createUser(username, first, last, password) {
+    try {
+        await prisma.user.create({
+            data: {
+                username: username,
+                first: first,
+                last: last,
+                password: password
+            },
+        });
+    }
+    catch (err) {
+        return err;
+    }
 }
 
 
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,17 @@
 const { Router } = require("express");
 const userRouter = Router();
 
-userRouter.post('/', (req, res) => {
-    // !! TBI
-  return res.send("CREATING NEW USER, TBI");
+const { createUser } = require('../models/userQueries');
+
+userRouter.post('/', async (req, res) => {
+    console.log(req.body)
+    // !! Need to add password hashing here, we should actually send back a token?
+    try {
+      await createUser(req.body.username, req.body.first, req.body.last, req.body.password);
+    } catch (err) {
+      res.sendStatus(500);
+    }
+    res.send("NEW USER CREATED");
 });
 
 
